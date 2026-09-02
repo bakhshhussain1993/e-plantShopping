30c123cf683d608ca9e75736b6a0591918227ac3
bakhshhussain1993 <bakhshhussain1993@gmail.com>
2026-09-02 12:20:34 +0500

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,20 @@
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>About Us</h1>
+
+      <p>
+        Welcome to e-plantShopping, your online destination for
+        beautiful and healthy plants.
+      </p>
+
+      <p>
+        Our mission is to make it easy for everyone to bring
+        nature into their homes by providing a wide variety of
+        quality plants and excellent customer service.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
